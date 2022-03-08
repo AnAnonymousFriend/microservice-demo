2a363acae76a187039a40e6146d611d57f8d4059
kai <871779972@qq.com>
2022-03-08 10:41:56 +0800

    1.7.0 release note

--- backend/backend.go
+++ backend/backend.go
@@ -45,7 +45,7 @@ func buildStamp(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	log.Println("Hello, welcome to the microservice world. welcome to zadig test1")
+	log.Println("Hello, welcome to the microservice world. welcome to zadig test 2")
 
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/api/buildstamp", buildStamp)
